572f2f5cd7c5fd25036ad25c73af74d6b4c13031
DmytroShHub <juristdima@gmail.com>
2025-06-28 15:32:19 +0300

    PasswordValidationException

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class UserService {
     public void registerUser(User user) {
-        PasswordValidator passwordValidator = new PasswordValidator;
+        PasswordValidator passwordValidator = new PasswordValidator();
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
             saveUser(user);
